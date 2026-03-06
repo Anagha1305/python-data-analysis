549eb62115cdbb2a14e0682f9cd1fe9677e13fbd
Anagha Kadam <anaghakadam1305@gmail.com>
2026-03-06 12:40:49 +0530

    First numpy marks analysis project

---
+++ marks_analysis.py
@@ -0,0 +1,8 @@
+import numpy as np
+marks = np.array([70,85,60,87,90])
+print("Marks:",marks)
+print("Average:",np.mean(marks))
+print("Max:",np.max(marks))
+print("Min:",np.min(marks))
+print("Total Sum:",np.sum(marks))
+
